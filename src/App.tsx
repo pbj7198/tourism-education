61f15a72ce67403ca6489b479631695927c9839c
pbj7198 <pbj7198>
2025-01-07 13:34:20 +0900

    페이지 전환 애니메이션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material';
+import { AnimatePresence } from 'framer-motion';
 import Navbar from './components/Navbar';
+import PageTransition from './components/PageTransition';
 import Home from './pages/Home';
 import About from './pages/About';
 import Notice from './pages/Notice';
@@ -22,20 +24,30 @@ const theme = createTheme({
   },
 });
 
+const AppRoutes = () => {
+  const location = useLocation();
+  
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageTransition><Home /></PageTransition>} />
+        <Route path="/about" element={<PageTransition><About /></PageTransition>} />
+        <Route path="/notice" element={<PageTransition><Notice /></PageTransition>} />
+        <Route path="/resources" element={<PageTransition><Resources /></PageTransition>} />
+        <Route path="/jobs" element={<PageTransition><Jobs /></PageTransition>} />
+        <Route path="/board" element={<PageTransition><Board /></PageTransition>} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <div>
           <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notice" element={<Notice />} />
-            <Route path="/resources" element={<Resources />} />
-            <Route path="/jobs" element={<Jobs />} />
-            <Route path="/board" element={<Board />} />
-          </Routes>
+          <AppRoutes />
         </div>
       </Router>
     </ThemeProvider>
