9d784834dd85459330a723d1ccf3fe6ce67a4ba8
pbj7198 <pbj7198>
2025-01-19 18:57:45 +0900

    style: 배경 이미지 제거 및 흰 배경으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,14 @@ import JobPostForm from './pages/JobPostForm';
 import JobPostDetail from './pages/JobPostDetail';
 import JobPostEdit from './pages/JobPostEdit';
 import Board from './pages/Board';
-import BoardForm from './pages/BoardForm';
-import BoardDetail from './pages/BoardDetail';
-import BoardEdit from './pages/BoardEdit';
-import Contact from './pages/Contact';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import AdminUsers from './pages/AdminUsers';
 import PostDetail from './pages/PostDetail';
 import PostForm from './pages/PostForm';
 import PostEdit from './pages/PostEdit';
-import TeachingMaterials from './pages/TeachingMaterials';
-import MaterialForm from './pages/MaterialForm';
-import MaterialDetail from './pages/MaterialDetail';
-import MaterialEdit from './pages/MaterialEdit';
+import BoardEdit from './pages/BoardEdit';
+import Contact from './pages/Contact';
 import AdminPosts from './pages/AdminPosts';
 
 const theme = createTheme({
@@ -40,6 +34,11 @@ const theme = createTheme({
       'sans-serif',
     ].join(','),
   },
+  palette: {
+    background: {
+      default: '#fff'
+    }
+  }
 });
 
 function App() {
@@ -62,10 +61,10 @@ function App() {
               sx={{ 
                 flexGrow: 1,
                 minHeight: 'calc(100vh - 64px)',
-                position: 'relative',
                 paddingTop: '64px',
                 display: 'flex',
                 flexDirection: 'column',
+                backgroundColor: '#fff'
               }}
             >
               <Routes>
@@ -78,20 +77,14 @@ function App() {
                 <Route path="/jobs/:id" element={<JobPostDetail />} />
                 <Route path="/jobs/:id/edit" element={<JobPostEdit />} />
                 <Route path="/board" element={<Board />} />
-                <Route path="/board/new" element={<BoardForm />} />
-                <Route path="/board/:id" element={<BoardDetail />} />
-                <Route path="/board/:id/edit" element={<BoardEdit />} />
-                <Route path="/contact" element={<Contact />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/admin/users" element={<AdminUsers />} />
                 <Route path="/posts/:id" element={<PostDetail />} />
                 <Route path="/posts/new" element={<PostForm />} />
                 <Route path="/posts/:id/edit" element={<PostEdit />} />
-                <Route path="/materials" element={<TeachingMaterials />} />
-                <Route path="/materials/new" element={<MaterialForm />} />
-                <Route path="/materials/:id" element={<MaterialDetail />} />
-                <Route path="/materials/:id/edit" element={<MaterialEdit />} />
+                <Route path="/board/:id/edit" element={<BoardEdit />} />
+                <Route path="/contact" element={<Contact />} />
                 <Route path="/admin/posts" element={<AdminPosts />} />
               </Routes>
             </Box>
