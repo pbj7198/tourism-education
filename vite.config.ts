fa217ab2a3d71125bdf94b28ea0c453eb54c62d9
pbj7198 <pbj7198>
2025-01-21 20:27:24 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  server: {
+    middleware: true
+  },
+  base: './',
   publicDir: 'public',
   define: {
     'process.env.PUBLIC_URL': '""',
